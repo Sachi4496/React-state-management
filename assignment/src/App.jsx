f0a7881b483d5753cf0e3a3e54e8e8abe1f4fa5c
Sachidanand Pradhan <sachidanand.kpradhan@gmail.com>
2021-12-24 22:46:10 +0530

    commit

---
+++ assignment/src/App.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/counter';
+console.log(Counter);
+
+function App() {
+const [count,setCount] = useState(0);
+
+const changeCount = (value) =>{
+  setCount(count + value);
+}
+const double = (value) =>{
+  setCount(count * value);
+}
+  return (
+    <div className="App">
+      <Counter changeCount={changeCount} double={double}  count={count} />
+    </div>
+  );
+}
+
+export default App;
